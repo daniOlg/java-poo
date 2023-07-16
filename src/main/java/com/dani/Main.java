b71e06819b7164926c4a0b12c910a6c6176feb9a
Daniel Olguín Martínez <dani.olg@hotmail.com>
2023-07-15 22:46:46 -0400

    write application requirements

--- src/main/java/com/dani/Main.java
+++ src/main/java/com/dani/Main.java
@@ -2,6 +2,41 @@ package com.dani;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        Requerimientos:
+        - Crear 5 estudiantes y generar notas al azar para cada asignatura
+        - Promediar notas del estudiante en cada asignatura y en general
+        - Mostrar notas y promedio en cada asignatura y en general
+        - Usar herencia
 
+        Pasos:
+        - Crear clases
+        - Crear constructores adecuados a la necesidad del problema
+        - Crear getters y setters necesarios
+        - Hacer pruebas con lo que tenemos hasta ahora
+        - Crear funciones que se adecuen a los requerimientos
+        - Probar funciones creadas
+        - Adecuar funciones de utilidad como estáticas
+
+        Vamos a necesitar:
+        - Crear carreras
+
+        - Crear cursos
+        - Asignar cursos a las carreras
+
+        - Crear asignaturas
+        - Asignar asignaturas a los cursos
+
+        - Crear profesores
+        - Asignar profesores a las asignaturas
+
+        - Crear estudiantes
+        - Asignar estudiantes a las carreras
+
+        Contemplar:
+        - Al asignar un estudiante a una carrera a este se le debe agregar a un curso aleatorio
+        - Al asignar un estudiante a un curso a este estudiante se le deben asignar todas las asignaturas del curso
+        - Al asignar una asignatura al estudiante deben crearse la cantidad de notas de la asignatura en la nota minima
+         */
     }
 }
